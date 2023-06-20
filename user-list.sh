dde40519cfef656404a25903bd0863dfc1931a00
reichan2 <rafijidan7@gmail.com>
2023-06-20 22:44:07 +0700

    first commit

---
+++ user-list.sh
@@ -0,0 +1,73 @@
+#wget https://github.com/${GitUser}/
+GitUser="Rerechan02"
+#link izin ip vps
+url_izin='https://raw.githubusercontent.com/Rerechan02/iziznscript/main/ip'
+
+# Mendapatkan IP VPS saat ini
+ip_vps=$(curl -s ifconfig.me)
+
+# Mendapatkan isi file izin.txt dari URL
+izin=$(curl -s "$url_izin")
+
+# Memeriksa apakah konten izin.txt berhasil didapatkan
+if [[ -n "$izin" ]]; then
+  while IFS= read -r line; do
+    # Memisahkan nama VPS, IP VPS, dan tanggal kadaluwarsa
+    nama=$(echo "$line" | awk '{print $1}')
+    ipvps=$(echo "$line" | awk '{print $2}')
+    tanggal=$(echo "$line" | awk '{print $3}')
+
+    # Memeriksa apakah IP VPS saat ini cocok dengan IP VPS yang ada di izin.txt
+    if [[ "$ipvps" == "$ip_vps" ]]; then
+      echo "Nama VPS: $nama"
+      echo "IP VPS: $ipvps"
+      echo "Tanggal Kadaluwarsa: $tanggal"
+      break
+    fi
+  done <<< "$izin"
+
+  # Memeriksa apakah IP VPS ditemukan dalam izin.txt
+  if [[ "$ipvps" != "$ip_vps" ]]; then
+    echo "IP VPS tidak ditemukan dalam izin.txt"
+    exit 0
+  fi
+else
+  echo "Konten izin.txt tidak berhasil didapatkan dari URL"
+  exit 0
+fi
+
+echo -e "\e[32mloading...\e[0m"
+clear 
+if [ -f /etc/debian_version ]; then
+	UIDN=1000
+elif [ -f /etc/redhat-release ]; then
+	UIDN=500
+else
+	UIDN=500
+fi
+clear
+echo " "
+echo " "
+echo "===========================================";
+echo " "
+echo "-----------------------------------"
+echo "        USER ACCOUNTS LIST         "
+echo "-----------------------------------"
+echo "[USERNAME]   -   [DATE EXPIRED]  "
+echo " "
+while read ceklist
+do
+        AKUN="$(echo $ceklist | cut -d: -f1)"
+        ID="$(echo $ceklist | grep -v nobody | cut -d: -f3)"
+        exp="$(chage -l $AKUN | grep "Account expires" | awk -F": " '{print $2}')"
+        if [[ $ID -ge $UIDN ]]; then
+        printf "%-17s %2s\n" "$AKUN" "$exp"
+        fi
+done < /etc/passwd
+JUMLAH="$(awk -F: '$3 >= '$UIDN' && $1 != "nobody" {print $1}' /etc/passwd | wc -l)"
+echo "-------------------------------------"
+echo "Number Of User Accounts: $JUMLAH USERS"
+echo "-------------------------------------"
+echo " "
+echo "===========================================";
+echo " ";
